4e3b28075c81bf76993599c03bd6f86af59d0e11
Alessandro Taufer <alexander141220@gmail.com>
2023-11-12 01:46:57 +0200

    Accept images as json

--- python_scripts/webserver.py
+++ python_scripts/webserver.py
@@ -1,15 +1,19 @@
-from bottle import app, route, run, request, response
+from bottle import app, route, run, request, response, BaseRequest
 import llm_extract_material_amount as llm_api
 import device_detector as detector
 from bottle_cors_plugin import cors_plugin
+import json
 
 # We allow every kind of CORS
 app = app()
 app.install(cors_plugin('*'))
 
+# We increase the maximum size of the request to allow the api to handle big images
+BaseRequest.MEMFILE_MAX = 1024 * 1024
+
 # We create the object used to interact with the llm
 llm_obj = llm_api.init_llm()
-
+print("Done")
 
 # Return the estimation of crm contained into the device passed as an argument
 @route('/materialApi')
@@ -28,18 +32,23 @@ def material_api():
 
 
 # Accepts an image as a parameter, detect the device in it and estimate the amount of crm
-@route('/detectorApi')
+@route('/detectorApi', method='POST')
 def material_api():
     response.content_type = "application/json"
 
+    # Get the image from the request
+    image = json.loads(request.body.read())['image']
+
     # Fail if the mandatory arguments have not been passed (base64 image)
-    if "image" not in request.params:
+    if image is None or image == "":
         response.status = 501
         return 'No image given'
 
-    device = detector.detect_device(request.params['image'])
+    device = detector.detect_device(image)
 
     # Ask the llm to estimate the content
+    # request.get("http://ec2-15-160-182-105.eu-south-1.compute.amazonaws.com:8080/api/getDeviceInfo?name=IphoneX")
+    # TODO the request should be redirected directed to the main api
     llm_response = llm_api.generate_api_info(device, llm_obj)
 
     return llm_response
